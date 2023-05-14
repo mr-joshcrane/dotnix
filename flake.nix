146b2be4e2227500184442fd0b35424ed81a0895
mr-joshcrane <57082182+mr-joshcrane@users.noreply.github.com>
2023-05-14 12:34:44 +0800

    minimal flake for ubuntu

--- flake.nix
+++ flake.nix
@@ -1,37 +1,31 @@
 {
-  description = "My first nix flake";
+  description = "A minimal flake for Ubuntu";
 
   inputs = {
-    # Package sets
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-22.11-darwin";
-    nixpkgs-unstable.url = github:NixOS/nixpkgs/nixpkgs-unstable;
-
-    # Environment/system management
-    darwin.url = "github:lnl7/nix-darwin/master";
-    darwin.inputs.nixpkgs.follows = "nixpkgs-unstable";
-
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
   };
-  
-  # add the inputs declared above to the argument attribute set
-  outputs = { darwin, home-manager, nixpkgs, ... }: {
 
-    darwinConfigurations."mini" = darwin.lib.darwinSystem {
-        system = "aarch64-darwin";
-        modules = [
-            home-manager.darwinModules.home-manager
-	    ./hosts/mini/config.nix
-        ]; 
-    };
+  outputs = { home-manager, nixpkgs, ... }: {
+    defaultPackage.x86_64-linux = with nixpkgs.legacyPackages.x86_64-linux; [
+      htop
+      neovim
+      curl
+      git
+    ];
 
-    darwinConfigurations."AU-L-0300" = darwin.lib.darwinSystem {
-        system = "aarch64-darwin";
-        modules = [
-            home-manager.darwinModules.home-manager
-            ./hosts/AU-L-0300/config.nix
+    homeManagerConfigurations.myUbuntu = home-manager.lib.homeManagerConfiguration {
+      configuration = { pkgs, ... }: {
+        home.packages = with pkgs; [
+          htop
+          neovim
+          curl
+          git
         ];
+      };
+      system = "x86_64-linux";
     };
-
   };
 }
