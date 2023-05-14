3aa89533046471c501c4c6d0483b9271993912f2
mr-joshcrane <57082182+mr-joshcrane@users.noreply.github.com>
2023-05-14 13:07:44 +0800

    get something up

--- flake.nix
+++ flake.nix
@@ -1,26 +1,20 @@
 {
-  description = "A minimal flake for Ubuntu";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.03";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-22.11-darwin";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
-  };
+  outputs = { self, nixpkgs }: {
 
-  outputs = { home-manager, nixpkgs, ... }: {
-    packages.x86_64-linux.default = home-manager.lib.homeManagerConfiguration {
-      configuration = { nixpkgs, ... }: {
-        home.packages = with nixpkgs; [
-          htop
-          neovim
-          curl
-          git
-        ];
-      };
+    nixosConfigurations.container = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      homeDirectory = "/home/joshcrane";
-      username = "joshcrane";
+      modules =
+        [ ({ pkgs, ... }: {
+            boot.isContainer = true;
+
+            # Let 'nixos-version --json' know about the Git revision
+            # of this flake.
+            system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
+
+        ];
     };
+
   };
 }
