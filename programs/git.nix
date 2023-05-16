2efeca02984a521fd1c7b0bc58c22e51a7090f4e
mr-joshcrane <57082182+mr-joshcrane@users.noreply.github.com>
2023-05-16 17:00:20 +0800

    fixing git config

--- programs/git.nix
+++ programs/git.nix
@@ -1,27 +1,6 @@
 { pkgs }:
 {
     enable = true;
-    userName = "Matt Tyler";
-    userEmail = "me@matthewtyler.io"
-    extraConfig = {
-      credentials.helper = "osxkeychain";
-      core = {
-        editor = "nvim";
-      };
-      color = {
-        ui = true;
-      };
-      push = {
-        default = "simple";
-      };
-      pull = {
-        ff = "only";
-      };
-      init = {
-        defaultBranch = "main";
-      };
-    };
-    ignores = [
-      ".DS_Store"
-    ];
+    userName = "Josh Crane";
+    userEmail = "josh.crane512@gmail.com"
 }
