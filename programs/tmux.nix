39cccf342db6d437cc56053c3bd72aa1b3b3096f
Josh <josh.crane512@gmail.com>
2023-06-13 22:01:13 +0800

    Starting with basic tmux

--- programs/tmux.nix
+++ programs/tmux.nix
@@ -1,25 +1,10 @@
 { pkgs, ... }:
-let
-  modern-tmux-theme = mkTmuxPlugin {
-  pluginName = "modern-tmux-theme";
-    version = "unstable-2023-01-06";
-    src = fetchFromGitHub {
-      owner = "Millrocious";
-      repo = "modern-tmux-theme";
-      rev = "29dad92c8a2486e5b6f116e42883906c00a1f0a2";
-      sha256 = "sha256-ymmCI6VYvf94Ot7h2GAboTRBXPIREP+EB33+px5aaJk=";
-    };
-  };
-in
 {
     enable = true;
     prefix = "C-a";
     terminal = "screen-256color";
 
     plugins = with pkgs; [
-      tmuxPlugins.better-mouse-mode
-      modern-tmux-theme
-      # tmuxPlugins.catppuccin
       tmuxPlugins.sensible
       tmuxPlugins.vim-tmux-navigator
     ];
