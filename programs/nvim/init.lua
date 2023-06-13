d5d9d27b5235436b496165cca2d12aaa1a2c51b5
Josh <josh.crane512@gmail.com>
2023-06-13 23:00:48 +0800

    nvim config

--- programs/nvim/init.lua
+++ programs/nvim/init.lua
@@ -10,3 +10,18 @@ vim.g.mapleader = " "
 vim.cmd [[autocmd FileType markdown set tw=80 wrap]]
 
 vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope find_files<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>fg', ':Telescope live_grep<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>fb', ':Telescope buffers<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>fh', ':Telescope help_tags<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>fs', ':Telescope lsp_document_symbols<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>fr', ':Telescope lsp_references<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>fd', ':Telescope lsp_definitions<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>fi', ':Telescope lsp_implementations<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>fc', ':Telescope lsp_code_actions<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>ft', ':Telescope treesitter<CR>', {noremap = true, silent = true})
+
+
+set tabstop=2
+set shiftwidth=2
+set expandtab
