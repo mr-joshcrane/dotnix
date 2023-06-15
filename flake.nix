b4d18abaf48974f2bc9d6cb1bfb5fcf49704f231
Josh Crane <josh.crane@cultureamp.com>
2023-06-15 11:34:55 +0800

    switching to aarch

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
 
   outputs = { nixpkgs, home-manager, ... }:
     let
-      system = "x86_64-linux";
+      system = "aarch64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
       homeConfigurations.jcrane = home-manager.lib.homeManagerConfiguration {
