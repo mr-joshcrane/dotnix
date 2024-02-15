b70ab72b38749994c019fc3559886a8e3537781d
mr-joshcrane <57082182+mr-joshcrane@users.noreply.github.com>
2024-02-15 12:34:57 +0800

    Update zsh.nix

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -5,6 +5,6 @@
     vi = "nvim";
     vim = "nvim";
   };
-  initExtra = ". ~/code/rolesanywherev4/cmd/env.sh && eval $(cd ~/code/rolesanywherev4 && go run cmd/main.go)
-  export PINECONE_URL=https://knowledge-cfef6dc.svc.gcp-starter.pinecone.io";
+  ## initExtra = ". ~/code/rolesanywherev4/cmd/env.sh && eval $(cd ~/code/rolesanywherev4 && go run cmd/main.go)
+  ## export PINECONE_URL=https://knowledge-cfef6dc.svc.gcp-starter.pinecone.io";
 }
