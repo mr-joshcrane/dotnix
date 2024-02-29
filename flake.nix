5e4eb10a0349c44d6004d1a7500ae101ed9d9d8c
mr-joshcrane <57082182+mr-joshcrane@users.noreply.github.com>
2024-02-29 18:44:52 +0800

    Switching back from M1

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
 
   outputs = { nixpkgs, home-manager, ... }:
     let
-      system = "aarch64-linux";
+      system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
       homeConfigurations.jcrane = home-manager.lib.homeManagerConfiguration {
