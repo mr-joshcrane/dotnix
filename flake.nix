554b49bac7e83a3729e3dd558bbc993a12f175cd
mr-joshcrane <57082182+mr-joshcrane@users.noreply.github.com>
2024-03-10 11:00:01 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
   outputs = { nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = nixpkgs.packages.${system};
     in {
       homeConfigurations.joshua = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
