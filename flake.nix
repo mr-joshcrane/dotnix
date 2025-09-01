f558f31cd6375bbae37c44ffdecf2c72de505129
Josh <josh.crane512@gmail.com>
2025-09-01 18:04:47 +0800

    Updating to remove dependency on home manager

--- flake.nix
+++ flake.nix
@@ -1,27 +1,62 @@
 {
-  inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux"; # change if needed
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true; # optional
+    };
+  in {
+    # install with: nix profile install .#
+    packages.${system}.default = pkgs.buildEnv {
+      name  = "josh-packages";
+      paths = with pkgs; [
+        # --- Shell & terminal ---
+        "zsh"
+        "starship"
+        "tmux"
+        "direnv"
+        "stow"
+
+        # --- VCS & repo tooling ---
+        "git"
+        "gh"
+        "lazygit"
+
+        # --- Core CLI utilities ---
+        "ripgrep"
+        "gnugrep"
+        "jq"
+        "yq-go"
+        "wget"
+        "unzip"
+
+        # --- Cloud / Ops / Security ---
+        "flyctl"
+        "yubikey-manager"
+
+        # --- Go toolchain ---
+        "go"
+        "gopls"
+        "delve"
+        "golangci-lint"
+
+        # --- Rust toolchain ---
+        "rustc"
+        "cargo"
+        "rust-analyzer"
+        "rustPackages.clippy"
+
+        # --- BEAM (Elixir/Erlang) ---
+        "elixir_1_16"
+        "erlang"
+
+        # --- Node (for Mason/LSP etc.) ---
+        "nodejs_22"
+      ];
     };
   };
-
-  outputs = { nixpkgs, home-manager, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      homeConfigurations.joshua = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ./home.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
-      };
-    };
 }
+
